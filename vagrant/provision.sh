64553d7119aa79a0f6a50f9846503b5e45b5ba3b
ChieftainY2k <ChieftainY2k@gmail.com>
2016-03-09 15:01:25 +0100

    update

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -20,7 +20,6 @@ cp /vagrant/VirtualHost/jenkins.conf /etc/apache2/sites-available
 a2ensite jenkins.conf
 service apache2 restart
 
-apt-get -y install git
 
 
 #############################################################
@@ -48,4 +47,50 @@ chown -R jenkins:nogroup *
 
 curl -X POST http://jenkins.local/reload
 /etc/init.d/jenkins restart
+
+#########
+# Install tools for PHP code
+#########
+
+apt-get -y install git
+apt-get -y install php5
+
+###
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+sudo mv phpunit.phar /usr/local/bin/phpunit
+
+###
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+chmod +x phpcs.phar
+sudo mv phpcs.phar /usr/local/bin/phpcs
+
+###
+curl -OL https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+chmod +x phpcbf.phar
+sudo mv phpcbf.phar /usr/local/bin/phpcbf
+
+###
+wget https://phar.phpunit.de/phploc.phar
+chmod +x phploc.phar
+sudo mv phploc.phar /usr/local/bin/phploc
+
+###
+wget -c http://static.phpmd.org/php/latest/phpmd.phar
+chmod +x phpmd.phar
+sudo mv phpmd.phar /usr/local/bin/phpmd
+
+###
+wget https://phar.phpunit.de/phpcpd.phar
+chmod +x phpcpd.phar
+sudo mv phpcpd.phar /usr/local/bin/phpcpd
+
+
+
+
+
+
+
+
+
 
